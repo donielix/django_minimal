8f3be1a1fadca1b5609a5fa99d42ef4396405046
Daniel Diego Horcajuelo <daniel.diego@gdsmodellica.com>
2022-08-07 22:39:31 +0000

    set-up celery apps

---
+++ root/dev_settings.py
@@ -0,0 +1,26 @@
+import os
+from .base_settings import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-e7z0g(o$6&)&(zr)vql(c(!7s+r=0=@ksohca7#$v&7^17iwc#"
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ["*"]
+
+# Database
+# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("POSTGRES_NAME"),
+        "USER": os.environ.get("POSTGRES_USER"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        "HOST": "db",
+        "PORT": 5432,
+    }
+}
+
+CELERY_BROKER_URL = f"amqp://{os.getenv('RABBITMQ_DEFAULT_USER')}:{os.getenv('RABBITMQ_DEFAULT_PASS')}@rabbitmq:5672"
